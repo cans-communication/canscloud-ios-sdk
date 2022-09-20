018c22e5cef3b2f0a11a66712be18150d9541e1c
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-09-20 13:34:32 +0700

    WIP: Updated private code

---
+++ TestAccess.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  canscloud-ios-sdk
+//
+//  Created by Siraphop Chaisirikul on 20/9/2565 BE.
+//
+
+import Foundation
+
+private class TestAccess {
+    private let dontSeeMe = "1234"
+}
